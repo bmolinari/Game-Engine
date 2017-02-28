95f392b5546b6129beaac24b9661327313ca4f1c
bmolinari <b.molinari@outlook.com>
2017-02-28 18:46:31 -0500

    Project now opens an empty OpenGL window. OpenGL libraries and .dll files included and linked.

---
+++ DevSpaceGame/GlWindow.h
@@ -0,0 +1,9 @@
+#ifndef GAME_GL_WINDOW_H
+#define GAME_GL_WINDOW_H
+#include <QtOpenGL\qglwidget>
+
+class GlWindow : public QGLWidget
+{
+};
+
+#endif
--- DevSpaceGame/Main.cpp
+++ DevSpaceGame/Main.cpp
@@ -1,9 +1,13 @@
 #include <qt\qapplication.h>
+#include <Qt\qwidget.h>
+#include "GlWindow.h"
 
 int main(int argc, char* argv[])
 {
 	QApplication application(argc, argv);
-
+	GlWindow myWindow;
+	
+	myWindow.show();
 	return application.exec();
 }
 
