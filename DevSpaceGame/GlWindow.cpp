6156f5368f4c5574647a075d564e20c7e9392f8b
bmolinari <b.molinari@outlook.com>
2017-03-06 15:20:36 -0500

    Added a game loop, now printing to the console over and over. Created a moc metadata file of GlWindow.h

--- DevSpaceGame/GlWindow.cpp
+++ DevSpaceGame/GlWindow.cpp
@@ -2,6 +2,10 @@
 #include <cassert>
 #include "GlWindow.h"
 
+#include <iostream>
+
+
+
 void GlWindow::initializeGL()
 {
 	GLenum errorCode = glewInit();
@@ -12,13 +16,16 @@ void GlWindow::initializeGL()
 
 	float verts[] =
 	{
-		+0.0f, +0.1,
+		+0.0f, +0.1f,
 		-0.1f, -0.1f,
 		+0.1f, -0.1f,
 	};
 
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verts),
 		verts, GL_STATIC_DRAW);
+
+	connect(&timer, SIGNAL(timeout()), this, SLOT(myUpdate()));
+	timer.start(0);
 }
 
 void GlWindow::paintGL()
@@ -27,4 +34,9 @@ void GlWindow::paintGL()
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
 	glDrawArrays(GL_TRIANGLES, 0, 3);
+}
+
+void GlWindow::myUpdate()
+{
+	std::cout << "Test" << std::endl;
 }
--- DevSpaceGame/GlWindow.h
+++ DevSpaceGame/GlWindow.h
@@ -1,13 +1,20 @@
 #ifndef GAME_GL_WINDOW_H
 #define GAME_GL_WINDOW_H
 #include <QtOpenGL\qglwidget>
+#include <Qt\qtimer.h>
 
 class GlWindow : public QGLWidget
 {
+	Q_OBJECT
+
 	GLuint vertexBufferID;
+	QTimer timer;
 protected:
 	void initializeGL();
 	void paintGL();
+private slots:
+	void myUpdate();
+
 };
 
 #endif
